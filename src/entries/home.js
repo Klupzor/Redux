0b3c6fb0a083d7b430c006c125465c4cdb7fa433
klupzor <lopezgand@gmail.com>
2018-05-30 21:08:49 -0500

    Iniciando con redux

--- src/entries/home.js
+++ src/entries/home.js
@@ -2,7 +2,24 @@ import React from 'react'
 import {render} from 'react-dom'
 import data from '../api.json'
 import Home from '../pages/containers/home';
+import { createStore } from 'redux';
 
+// redux ...................................................
+const initialState = {
+    data: {
+        ...data
+    }
+}
+
+const store = createStore(
+    (state)=> state,
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+console.log(store.getState())
+
+// redux .............................................................
 
 const homeContainer = document.getElementById('home-container')
 // ReactDOM.render(que voy a renderizar, donde lo hare)
